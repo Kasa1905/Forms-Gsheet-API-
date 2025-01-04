da51480286fed27e8855f396e3fe200dacd30cee
Kaushik <sambekaushik@gmail.com>
2025-01-04 13:27:49 +0530

    Delete API not needed directory

--- API not needed/src/lib/validation.ts
+++
@@ -1,6 +0,0 @@
-export function validatePassword(password: string): string | undefined {
-  if (password.length < 6) {
-    return 'Password must be at least 6 characters long';
-  }
-  return undefined;
-}
